c68c8edb68f8cec02bf94bfe9e790683bbd7bbed
Jan Schulz <jan.schulz@cileria.com>
2020-02-28 12:33:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## NodeServer 1
+
+This NodeJS server is an educational NodeJS backend for my React Online Course.
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const randomstring = require('randomstring');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/somedata', function(req, res) {
+    return res.send({
+        result: randomstring.generate(20)
+    });
+});
+
+app.post('/somedata', function(req, res) {
+    if(!req.body.data) {
+        return res.send({ error: 'data is required' });
+    }
+
+    return res.send({
+        somedata: 'posted: ' + randomstring.generate(20),
+    });
+});
+
+app.delete('/somedata/:id', function(req, res) {
+    return res.send({
+        somedata: 'deleted: ' + randomstring.generate(20),
+    });
+});
+
+
+app.listen( 3000 );
